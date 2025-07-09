325c7ceceb7bf26e1bb6bcf69f7288b61f292d7e
golingosite <algezoly2020@gmail.com>
2025-07-09 22:56:05 +0300

    first commit

---
+++ sine_wave.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Your SINE_TABLE values
+
+
+sine_values_voltage = [2.51, 3.27, 3.98, 4.53, 4.88, 5.0, 4.88, 4.53, 3.98, 3.27, 2.51, 1.75, 1.04, 0.49, 0.14, 0.02, 0.14, 0.49, 1.04, 1.75, 2.51]
+
+sample_period_us = 20
+time_points_us = [i * sample_period_us for i in range(len(sine_values_voltage))]
+
+plt.figure(figsize=(10, 6))
+plt.plot(time_points_us, sine_values_voltage, linestyle='-', drawstyle='steps-pre')
+plt.xlabel('Time')
+plt.ylabel('Voltage')
+plt.grid(True)
+plt.xticks(np.arange(0, max(time_points_us) + 1, 100))
+plt.yticks(np.arange(-2, 7, 0.5))
+plt.ylim(-2, 7)
+plt.show()
