0628cf56a17f7d961533da405a60e2398dbcfbc9
Elgizouli Mohamed <63818969+algezoly7@users.noreply.github.com>
2025-07-10 21:37:52 +0300

    Update sine_wave.py

--- sine_wave.py
+++ sine_wave.py
@@ -5,11 +5,6 @@ import pandas as pd
 df = pd.read_csv("C:/Users/algez/Downloads/design_project/sine_voltages.csv")
 sine_values_voltage = df['values'].values.tolist()
 
-sine_values_voltage2 = [2.51, 3.27, 3.98, 4.53, 4.88, 5.0, 4.88, 4.53, 3.98, 3.27, 2.51, 1.75, 1.04, 0.49, 0.14, 0.02, 0.14, 0.49, 1.04, 1.75, 2.51]
-
-if(sine_values_voltage == sine_values_voltage2):
-    print(True)
-
 sample_period_us = 20
 time_points_us = [i * sample_period_us for i in range(len(sine_values_voltage))]
 
